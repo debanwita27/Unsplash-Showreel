0810bf5e5cbeb43f9f5b4a44f6e679a2c454b9e0
Debanwita Mahato <debanwitamahato27@gmail.com>
2023-05-05 15:54:02 +0530

    removed commented code

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -2,7 +2,6 @@ import { AbsoluteFill, Img, staticFile } from 'remotion'
 
 // A static background image that lasts for the entire duration of the video.
 export default function BackgroundSequence(): JSX.Element {
-  // return <AbsoluteFill style={{ background: "linear-gradient(to top, #B19CD8, #E6D1F2)" }} />
   return (<AbsoluteFill>
     <Img
       src={staticFile('assets/bg-img.jpg')}
